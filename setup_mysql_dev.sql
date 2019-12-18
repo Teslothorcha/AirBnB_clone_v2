285078c1af679f417a394c01ef5262d814e539e0
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-17 22:08:17 -0500

    [Fix] databases name with wrong syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
---creates new user with privileges
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
-CREATE IF NOT EXISTS USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
-GRANT SELECT  ON performance_schema. * TO 'hbnb_dev'@'localhost';
+-- creates new user with privileges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db` . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT  ON `performance_schema` . * TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
