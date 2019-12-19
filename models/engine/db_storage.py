7f7ccd6392e5854b6cecfdb5b90f47e2e134d1b9
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-19 10:42:53 -0500

    [Fix] missing db update and place base inheritance

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -34,7 +34,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        class_list = [State, City, User]
+        class_list = [State, City, User, Place]
         dict_ = {}
 
         if cls is None:
