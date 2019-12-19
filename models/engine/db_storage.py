bad8f25e7b9b6d53b304741f247f3ae36a258ecf
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-19 01:02:59 -0500

    [Feature] implement db storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+new engine
+on db->storage
+"""
+from models.base_model import Base, BaseModel
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        db_user = getenv('HBNB_MYSQL_USER')
+        db_password = getenv('HBNB_MYSQL_PWD')
+        db_host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
+                                      .format(db_user,
+                                              db_password,
+                                              db_host,
+                                              db), pool_pre_ping=True)
+        if getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        class_list = [State, City]
+        dict_ = {}
+
+        if cls is None:
+            for clas in class_list:
+                objs = self.__session.query(clas).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    dict_[key] = obj
+        else:
+            objs = self.__session.query(cls).all()
+
+            for obj in objs:
+                key = obj.__class__.__name__ + '.' + obj.id
+                dict_[key] = obj
+
+        return dict_
+
+    def new(self, obj):
+        """ add object
+        to currnet
+        db session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """ stage all changes
+        to session db"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ deletes
+        object from db session
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ reload  from
+        db to session """
+        Base.metadata.create_all(self.__engine)
+        session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = session()
--- models/state.py
+++ models/state.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
+
+    @property
+    def cities(self):
+        return(self.cities)
