bf8673e1f11f443c392f0ffa1fa876b0142eac74
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-03 20:40:42 -0500

    [Fix] missing fab format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ create a tar file """
 
-
-import tarfile
+from fabric.api import local
 import os
 from datetime import datetime
 
@@ -11,14 +10,13 @@ def do_pack():
     """ check files an create actual tar"""
     dirr = "versions/"
     date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    f_name = web_static + date_time + '.tgz'
+    f_name = "web_static" + date_time + '.tgz'
     final_file = dirr + f_name
-
-    if not os.path.exists(dirr):
-        os.mkdir(dirr)
-    with tarfile.open(dirr + f_name, "w:gz") as tar:
-        tar.add("web_static", arcname=os.path.basename("web_static"))
-    if os.path.exists("final_file"):
+    try:
+        if not os.path.exists("versions"):
+            print("aca")
+            local("mkdir versions")
+        local("tar -zcvf {tar_name} web_static".format(tar_name=final_file))
         return final_file
-    else:
+    except:
         return None
