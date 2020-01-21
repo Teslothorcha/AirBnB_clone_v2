bfecd4a6319cc9cd4a23e3e3548bf920d0a9de59
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-21 12:22:12 -0500

    [Feature] flask variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ first route message"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ second route message"""
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ third route message"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run()
