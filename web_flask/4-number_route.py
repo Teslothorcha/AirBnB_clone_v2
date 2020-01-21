9dfabf25d95e28949e61f414422250e51223792a
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-21 13:28:55 -0500

    [Feature] number http

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ first route message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ second route message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ third route message"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ third route message"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def python_is_int(n):
+    """ third route message"""
+    if n.isdigit():
+        return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run()
