af9493a114fe13cbed5aa58df54cee748cf9ffb8
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-21 14:18:45 -0500

    [Feature] conditional on html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ first route message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ second route message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ third route message"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ third route message"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def python_is_int(n):
+    """ third route message"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def first_render(n):
+    """ third route message"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    """ third route message"""
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run()
