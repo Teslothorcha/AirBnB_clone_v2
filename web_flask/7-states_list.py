b64ecd1ef1bc11817335564548540b8e5507cfec
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-22 11:09:06 -0500

    [Fix] fuck it

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,21 @@
 from models import storage
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    lis = [storage.all('State')[st] for st in storage.all('State')]
+    lis = storage.all("State").values()
     return render_template('7-states_list.html', lis=lis)
 
 
 @app.teardown_appcontext
-def close_(self):
+def shutdown_(self):
     storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
