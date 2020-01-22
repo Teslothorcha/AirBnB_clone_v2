a90acdc0c72d9a44da5815fc4a598aaf3e35241f
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-21 23:26:13 -0500

    [Fix] fuck it

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     lis = [storage.all('State')[st] for st in storage.all('State')]
-    return render_template('7-states_list.html', lis=lista)
+    return render_template('7-states_list.html', lis=lis)
 
 
 @app.teardown_appcontext
